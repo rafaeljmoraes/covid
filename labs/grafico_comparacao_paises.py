8f8cf7f1a47b56f79a708b853e1fb6f4b625bafe
ericolisboa <erclsb2@gmail.com>
2020-03-21 11:32:15 -0300

    Melhoria no teste. Mostra um gráfico com vários países, no momento reforçando o Brasil. O código ainda pode melhorar bem.

--- labs/grafico_comparacao_paises.py
+++ labs/grafico_comparacao_paises.py
@@ -12,9 +12,9 @@ def get_covid_data():
     url = "https://covid.ourworldindata.org/data/ecdc/full_data.csv"
     return pd.read_csv(url)
 
-def build_days_column(df):
+def build_days_column(df, minimal_cases = 1):
     df = df.loc[:, ['date', 'location', 'total_cases']]
-    df = df[df['total_cases'] > 0]
+    df = df[df['total_cases'] >= minimal_cases]
     df['days'] = 0
     
     countries = df['location'].drop_duplicates()
@@ -25,8 +25,29 @@ def build_days_column(df):
     return df
 
 if __name__ == '__main__':
+    minimal_cases = 100
+    show_from = 2000
     df = get_covid_data()
-    df = build_days_column(df)
+    df = build_days_column(df, minimal_cases)
+
+    # Get countries to show (remove World data)
+    country_df = df[df['total_cases'] >= show_from]
+    countries = country_df['location'].drop_duplicates()
+    countries = countries.where(countries != 'World').dropna()
+
+    # Plot curves
+    for country in countries:
+        country_df = df[df['location'] == country]
+        plt.plot(country_df['days'], country_df['total_cases'])
+        
+    # plot Brazil
+    if 'Brazil' not in countries:
+        country_df = df[df['location'] == 'Brazil']
+        plt.plot(country_df['days'], country_df['total_cases'], marker='o', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4)
     
-    country_df = df[df['location'] == 'Brazil']
-    plt.plot(country_df['days'], country_df['total_cases'])
+    # Add plot information
+    plt.title('Casos a partir do %dº caso' % minimal_cases)
+    plt.xlabel('Dias')
+    plt.ylabel('Casos')
+    plt.yscale("log")
+    plt.legend(countries)
