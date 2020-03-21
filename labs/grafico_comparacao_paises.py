afa0ca6599e026e737f999064d96228120df52e1
ericolisboa <erclsb2@gmail.com>
2020-03-21 13:59:55 -0300

    Inclusão da opção para normalizar as curvas pela população do país. Algumas outras melhorias no código, com a criação de uma função para plotar a curva de um país.
    Não encontrei um arquivo de dados de população disponível na internet, acabei baixando um, editando e adicionando no repositório. Está bem ruim ainda, os dados são de 2014 e os nomes dos países estão em português.

--- labs/grafico_comparacao_paises.py
+++ labs/grafico_comparacao_paises.py
@@ -8,6 +8,35 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def get_country_population(country):
+    url = "100005.csv"
+    df = pd.read_csv(url, sep=';')
+
+    from_to = {}
+    from_to['Brazil'] = 'Brasil'
+    from_to['Austria'] = 'Áustria'
+    from_to['Belgium'] = 'Bélgica'
+    from_to['France'] = 'França'
+    from_to['Germany'] = 'Alemanha'
+    from_to['Iran'] = 'Irã'
+    from_to['Italy'] = 'Itália'
+    from_to['Netherlands'] = 'Países Baixos'
+    from_to['South Korea'] = 'Coréia do Sul'
+    from_to['Spain'] = 'Espanha'
+    from_to['Switzerland'] = 'Suíça'
+    from_to['United Kingdom'] = 'Reino Unido'
+    from_to['United States'] = 'Estados Unidos'
+    
+    if country in from_to.keys():
+        country = from_to[country]
+    #print(df)
+    result = df[df['País'] == country]['População']
+    if result.empty:
+        return 0
+    else:
+        return int(result)
+    
+    
 def get_covid_data():
     url = "https://covid.ourworldindata.org/data/ecdc/full_data.csv"
     return pd.read_csv(url)
@@ -24,9 +53,27 @@ def build_days_column(df, minimal_cases = 1):
         df.loc[index, 'days'] = range(1, index.size + 1)
     return df
 
+def plot_country(df, country, normalize, emphasis):
+    country_df = df[df['location'] == country]
+    total_cases = country_df['total_cases']
+    plot = True
+    if normalize:
+        population = get_country_population(country)
+        if population > 0:
+            total_cases = total_cases / population
+        else:
+            plot = False
+    if plot:
+        if emphasis:
+            plt.plot(country_df['days'], total_cases, marker='o', markersize=12, linewidth=4)
+        else:
+            plt.plot(country_df['days'], total_cases)
+    return plot
+
 if __name__ == '__main__':
     minimal_cases = 100
     show_from = 2000
+    normalize = True
     df = get_covid_data()
     df = build_days_column(df, minimal_cases)
 
@@ -36,18 +83,19 @@ if __name__ == '__main__':
     countries = countries.where(countries != 'World').dropna()
 
     # Plot curves
+    legend_curves = []
     for country in countries:
-        country_df = df[df['location'] == country]
-        plt.plot(country_df['days'], country_df['total_cases'])
-        
+        if plot_country(df, country, normalize, False):
+            legend_curves.append(country)
+            
     # plot Brazil
     if 'Brazil' not in countries:
-        country_df = df[df['location'] == 'Brazil']
-        plt.plot(country_df['days'], country_df['total_cases'], marker='o', markerfacecolor='blue', markersize=12, color='skyblue', linewidth=4)
+        if plot_country(df, 'Brazil', normalize, True):
+            legend_curves.append('Brazil')
     
     # Add plot information
     plt.title('Casos a partir do %dº caso' % minimal_cases)
     plt.xlabel('Dias')
     plt.ylabel('Casos')
     plt.yscale("log")
-    plt.legend(countries)
+    plt.legend(legend_curves)
