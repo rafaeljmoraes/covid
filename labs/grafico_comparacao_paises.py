b1cc2185b58555b6bcdec1ec6cca470c475e3b16
ericolisboa <erclsb2@gmail.com>
2020-03-21 10:34:58 -0300

    Criação da função para limpeza da base de dados (tira as datas com zero casos e remoção de colunas). A função cria a coluna 'days', com o número de dias desde o primeiro caso.
    Teste traçando um gráfico do Brasil.

--- labs/grafico_comparacao_paises.py
+++ labs/grafico_comparacao_paises.py
@@ -5,10 +5,28 @@ Created on Sat Mar 21 09:43:06 2020
 @author: Erico
 """
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 def get_covid_data():
     url = "https://covid.ourworldindata.org/data/ecdc/full_data.csv"
     return pd.read_csv(url)
 
-a = get_covid_data()
-print(a)
+def build_days_column(df):
+    df = df.loc[:, ['date', 'location', 'total_cases']]
+    df = df[df['total_cases'] > 0]
+    df['days'] = 0
+    
+    countries = df['location'].drop_duplicates()
+    for country in countries:
+        country_df = df[df['location'] == country]
+        index = country_df.index
+        df.loc[index, 'days'] = range(1, index.size + 1)
+    return df
+
+if __name__ == '__main__':
+    df = get_covid_data()
+    df = build_days_column(df)
+    
+    country_df = df[df['location'] == 'Brazil']
+    plt.plot(country_df['days'], country_df['total_cases'])
