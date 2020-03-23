3d100138375f8101f0d298725ef6518f5355c84f
ericolisboa <erclsb2@gmail.com>
2020-03-22 21:09:11 -0300

    Criação da classe Plot, englobando várias funções desenvolvidas anteriormente.

---
+++ labs/Plot.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 22 20:37:55 2020
+
+@author: Erico
+"""
+
+import matplotlib.pyplot as plt
+
+class Plot:
+    def __init__(self, covid_database):
+        self._covid_database = covid_database
+        plt.figure()
+
+    def _plot_curve(self, country, show_from, normalize, emphasis):
+        df = self._covid_database.get_data()
+        
+        country_df = df[df['location'] == country]
+        total_cases = country_df['total_cases']
+        plot = True
+        if normalize:
+            population = self._covid_database.get_country_population(country)
+            if population > 0:
+                total_cases = total_cases / population
+                if total_cases.max() < show_from:
+                    plot = False
+            else:
+                plot = False
+        else:
+            if total_cases.max() < show_from:
+                plot = False
+        if country == 'Brazil':
+            plot = True
+            print("%s, %.2f%%" % (country, total_cases.max()))
+        if plot:
+            if emphasis:
+                plt.plot(country_df['days'], total_cases, marker='o', markersize=12, linewidth=4)
+            else:
+                plt.plot(country_df['days'], total_cases)
+        return plot
+
+    def create_plot(self, minimal_cases, show_from, normalize, title=None, export_file=''):
+        df = self._covid_database.get_data()
+        
+        # Get countries to show (remove World data)
+        include = ['Brazil']
+        countries = df['location'].drop_duplicates()
+        if normalize:
+            include.append('World')
+        else:
+            countries = countries.where(countries != 'World').dropna()
+    
+        # Plot curves
+        legend_curves = []
+        for country in countries:
+            if country not in include:
+                if self._plot_curve(country, show_from, normalize, country in include):
+                    legend_curves.append(country)
+                
+        # plot included countries
+        for country in include:
+            if self._plot_curve(country, 0, normalize, True):
+                legend_curves.append(country)
+        
+        # Add plot information
+        if title:
+            plt.title(title)
+        else:
+            if normalize:
+                plt.title('Casos a partir do %dº caso\n(normalizados pela população)' % minimal_cases)
+            else:
+                plt.title('Casos a partir do %dº caso' % minimal_cases)
+        plt.xlabel('Dias')
+        plt.ylabel('Casos')
+        plt.yscale("log")
+        plt.legend(legend_curves)
+        if export_file:
+            plt.savefig(export_file)
+        
--- labs/grafico_comparacao_paises.py
+++ labs/grafico_comparacao_paises.py
@@ -4,86 +4,22 @@ Created on Sat Mar 21 09:43:06 2020
 
 @author: Erico
 """
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 
 from CovidDatabase import CovidDatabase
+from Plot import Plot
 
-
-def plot_country(covid_database, country, show_from, normalize, emphasis):
-    df = covid_database.get_data()
-    country_df = df[df['location'] == country]
-    total_cases = country_df['total_cases']
-    plot = True
-    if normalize:
-        population = covid_database.get_country_population(country)
-        if population > 0:
-            total_cases = total_cases / population
-            if total_cases.max() < show_from:
-                plot = False
-        else:
-            plot = False
-    else:
-        if total_cases.max() < show_from:
-            plot = False
-    if country == 'Brazil':
-        plot = True
-        print("%s, %.2f%%" % (country, total_cases.max()))
-    if plot:
-        if emphasis:
-            plt.plot(country_df['days'], total_cases, marker='o', markersize=12, linewidth=4)
-        else:
-            plt.plot(country_df['days'], total_cases)
-    return plot
-
-def create_plot(covid_database, minimal_cases, show_from, normalize, title=None, export_file=''):
-    df = covid_database.get_data()
-    
-    # Create new plot
-    plt.figure()
-
-    # Get countries to show (remove World data)
-    include = ['Brazil']
-    countries = df['location'].drop_duplicates()
-    if normalize:
-        include.append('World')
-    else:
-        countries = countries.where(countries != 'World').dropna()
-
-    # Plot curves
-    legend_curves = []
-    for country in countries:
-        if country not in include:
-            if plot_country(covid_database, country, show_from, normalize, country in include):
-                legend_curves.append(country)
-            
-    # plot included countries
-    for country in include:
-        if plot_country(covid_database, country, 0, normalize, True):
-            legend_curves.append(country)
-    
-    # Add plot information
-    if title:
-        plt.title(title)
-    else:
-        if normalize:
-            plt.title('Casos a partir do %dº caso\n(normalizados pela população)' % minimal_cases)
-        else:
-            plt.title('Casos a partir do %dº caso' % minimal_cases)
-    plt.xlabel('Dias')
-    plt.ylabel('Casos')
-    plt.yscale("log")
-    plt.legend(legend_curves)
-    if export_file:
-        plt.savefig(export_file)
               
 if __name__ == '__main__':
     covid_database = CovidDatabase()
     minimal_cases = 100
     covid_database.build_days_column(minimal_cases)
     df = covid_database.get_data()
-    
-    create_plot(covid_database, minimal_cases, 5e-4, True, None, 'normalized.png')
-    create_plot(covid_database, minimal_cases, 2.3e-4, True, None, 'normalized2.png')
-    create_plot(covid_database, minimal_cases, 2000, False, None, 'absolute.png')
+
+    plot1 = Plot(covid_database)
+    plot1.create_plot(minimal_cases, 5e-4, True, None, 'normalized.png')
+
+    plot2 = Plot(covid_database)
+    plot2.create_plot(minimal_cases, 2.3e-4, True, None, 'normalized2.png')
+
+    plot3 = Plot(covid_database)
+    plot3.create_plot(minimal_cases, 2000, False, None, 'absolute.png')
