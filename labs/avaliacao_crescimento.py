2601a9894eba42647fae628025f9303518d436f6
ericolisboa <erclsb2@gmail.com>
2020-03-26 22:11:10 -0300

    Reorganização do código, criando algumas funções.

--- labs/avaliacao_crescimento.py
+++ labs/avaliacao_crescimento.py
@@ -5,15 +5,77 @@ Created on Sat Mar 21 09:43:06 2020
 @author: Erico
 """
 
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 from CovidDatabase import CovidDatabase
 
+def get_forecast(df, start, ndays, forecast, countries = [], country_groups = []):
+    x = range(start, ndays + forecast + 1)
+
+    # Cria cenarios P10, P50 e P90
+    forecast_columns = ['Dias', 'Cenario', 'Taxa_crescimento']
+    df_forecast = pd.DataFrame(columns=forecast_columns)
+    data = []
+    for t in x:
+        growth = df[df['days'] == t]['growth']
+        data.append([t, 'P10', np.percentile(growth, 10)])
+        data.append([t, 'P50', np.percentile(growth, 50)])
+        data.append([t, 'P90', np.percentile(growth, 90)])
+    df_forecast = df_forecast.append(pd.DataFrame(data, columns=forecast_columns))
+
+    # Cria cenarios dos países
+    for country in countries:
+        country_df = df[df['location'] == country]
+        country_df = country_df[country_df['days'] <= ndays + forecast]
+        country_df = country_df[country_df['days'] >= start]
+        df_forecast = df_forecast.append(pd.DataFrame({'Dias': country_df['days'],
+                                                       'Cenario': country,
+                                                       'Taxa_crescimento': country_df['growth']}))
+
+    return df_forecast
+        
+
+def plot_growth(df, scenario_list, max_days):
+    plt.figure()
+    for scenario in scenario_list:
+        scenario_df = df[df['Cenario'] == scenario]
+        scenario_df = scenario_df[scenario_df['Dias'] <= max_days]
+        plt.plot(scenario_df['Dias'], scenario_df['Taxa_crescimento'])
+    plt.legend(scenario_list)
+
+
+def plot_brazil_forecast(df, brazil_df, scenario_list, max_days):
+    plt.figure()
+    ncases = brazil_df['total_cases'].max()
+    ndays = brazil_df['days'].max()
+    for scenario in scenario_list:
+        scenario_df = df[df['Cenario'] == scenario]
+        days = scenario_df['Dias'].to_numpy()
+        growth = scenario_df['Taxa_crescimento'].to_numpy()
+        x = []
+        y = []
+        for i in range(len(days)):
+            if days[i] <= ndays or days[i] > max_days:
+                pass
+            elif days[i] == ndays + 1:
+                x.append(days[i])
+                y.append(ncases * (1 + growth[i]))
+            else:
+                x.append(days[i])
+                y.append(y[-1] * (1 + growth[i]))
+        plt.plot(x, y)
+    plt.plot(brazil_df['days'], brazil_df['total_cases'], 'ko')
+    plt.legend(scenario_list + ['Brazil'])
+    plt.xlabel('Dias')
+    plt.ylabel('Casos')
+    
 if __name__ == '__main__':
     covid_database = CovidDatabase()
     minimal_cases = 100
-    forecast = 7 #days
+    forecast = 30 #days
+    plot_forecast = 15
     start = 5 #days
     covid_database.build_days_column(minimal_cases)
     df = covid_database.get_data()
@@ -23,95 +85,16 @@ if __name__ == '__main__':
     brazil_df = df[df['location'] == 'Brazil']
     brazil_df = brazil_df[brazil_df['days'] >= start]
     ndays = brazil_df['days'].max()
-    x = range(start, ndays + forecast + 1)
+    max_days = ndays + plot_forecast
+
+    countries = ['Iran', 'Italy', 'China', 'France', 'Germany', 'Japan', 
+                 'Australia', 'Spain', 'United States', 'Austria', 'Belgium', 
+                 'Netherlands', 'South Korea', 'Switzerland', 'United Kingdom']
+    df_forecast = get_forecast(df, start, ndays, forecast, countries)
+    df_forecast.to_csv('previsao.csv', index=False)
+
+    plot_growth(df_forecast, ['P10', 'P50', 'P90'], max_days)
+    plot_growth(df_forecast, countries, max_days)
     
-    p0 = []
-    p1 = []
-    p10 = []
-    p50 = []
-    p90 = []
-    p99 = []
-    p100 = []
-    length = []
-    for t in x:
-        growth = df[df['days'] == t]['growth']
-        p0.append(growth.min())
-        p10.append(np.percentile(growth, 10))
-        p50.append(np.percentile(growth, 50))
-        p90.append(np.percentile(growth, 90))
-        p100.append(growth.max())
-        length.append(len(growth))
-        
-    plt.figure()
-    plt.plot(x, p0, 'k--')
-    plt.plot(x, p10, 'c')
-    plt.plot(x, p50, 'y')
-    plt.plot(x, p90, 'c')
-    plt.plot(x, p100, 'k--')
-    plt.plot(brazil_df['days'], brazil_df['growth'], 'ko')
-    plt.xlabel('Dias')
-    plt.ylabel('Crescimento')
-    plt.savefig('growth_percentiles.png')
-    
-    plt.figure()
-    plt.plot(x, length)
-    plt.savefig('number_of_countries.png')
-
-    plt.figure()
-    countries = ['Iran', 'Italy', 'China', 'France', 'Germany', 'Japan', 'Australia']
-    for country in countries:
-        country_df = df[df['location'] == country]
-        country_df = country_df[country_df['days'] <= ndays + forecast]
-        country_df = country_df[country_df['days'] >= start]
-        plt.plot(country_df['days'], country_df['growth'])
-
-    plt.plot(brazil_df['days'], brazil_df['growth'], 'ko')
-    plt.legend(countries + ['Brazil'])
-    plt.xlabel('Dias')
-    plt.ylabel('Número de países')
-    plt.savefig('growth_countries.png')
-
-    # FORECAST 1
-    x = range(ndays + 1, ndays + forecast + 1)
-    growth_p10 = []
-    growth_p50 = []
-    growth_p90 = []
-    ncases_p10 = []
-    ncases_p50 = []
-    ncases_p90 = []
-    ncases = brazil_df['total_cases'].max()
-    for t in x:
-        growth = df[df['days'] == t]['growth']
-        aux_p10 = np.percentile(growth, 10)
-        aux_p50 = np.percentile(growth, 50)
-        aux_p90 = np.percentile(growth, 90)
-        growth_p10.append(aux_p10)
-        growth_p50.append(aux_p50)
-        growth_p90.append(aux_p90)
-        if t == x[0]:
-            ncases_p10.append(ncases * (1 + aux_p10))
-            ncases_p50.append(ncases * (1 + aux_p50))
-            ncases_p90.append(ncases * (1 + aux_p90))
-        else:
-            ncases_p10.append(ncases_p10[-1] * (1 + aux_p10))
-            ncases_p50.append(ncases_p50[-1] * (1 + aux_p50))
-            ncases_p90.append(ncases_p90[-1] * (1 + aux_p90))
-    plt.figure()
-    plt.plot(x, growth_p10, 'r')
-    plt.plot(x, growth_p50, 'g')
-    plt.plot(x, growth_p90, 'b')
-    plt.plot(brazil_df['days'], brazil_df['growth'], 'ko')
-    plt.legend(['P10', 'P50', 'P90', 'Brazil'])
-    plt.xlabel('Dias')
-    plt.ylabel('Crescimento')
-    plt.savefig('forecast1_growth.png')
-
-    plt.figure()
-    plt.plot(x, ncases_p10, 'r')
-    plt.plot(x, ncases_p50, 'g')
-    plt.plot(x, ncases_p90, 'b')
-    plt.plot(brazil_df['days'], brazil_df['total_cases'], 'ko')
-    plt.legend(['P10', 'P50', 'P90', 'Brazil'])
-    plt.xlabel('Dias')
-    plt.ylabel('Casos')
-    plt.savefig('forecast1_cases.png')
+    plot_brazil_forecast(df_forecast, brazil_df, ['P10', 'P50', 'P90'], max_days)
+    plot_brazil_forecast(df_forecast, brazil_df, countries, max_days)
